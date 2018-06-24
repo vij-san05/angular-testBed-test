b7a80a21359964447729887cf2d1540449ef1e7b
Vijay <vij.san05@outlook.com>
2018-06-24 22:30:03 +0530

    some change

--- src/app/highlight.directive.spec.ts
+++ src/app/highlight.directive.spec.ts
@@ -27,4 +27,21 @@ describe('HighlightDirective', () => {
     fixture = TestBed.createComponent(DirectiveHostComponent);
     fixture.detectChanges(); 
   });
+
+  it('should highlight the first element with cyan',()=>{
+    fixture.detectChanges();
+    let de = fixture.debugElement.queryAll(By.css('p'))[0];
+    //let el : HTMLElement = de.nativeElement;
+    //expect(el.style.backgroundColor).toBe('cyan');
+
+    expect(de.nativeElement.style.backgroundColor).toBe('cyan');
+  });
+
+  it('should highlight the second element with yellow  ', ()=>{
+    let de = fixture.debugElement.queryAll(By.css('p'))[1];
+    //let el : HTMLElement = de.nativeElement;
+    //expect(el.style.backgroundColor).toBe('cyan');
+
+    expect(de.nativeElement.style.backgroundColor).toBe('yellow');
+  });
 });
